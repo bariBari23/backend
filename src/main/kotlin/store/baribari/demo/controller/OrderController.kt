b570c0bea48ad6a3d53e79ce896d06f4bb23f2a2
mirageoasis <kimhw0820@naver.com>
2023-06-01 23:49:02 +0900

    feat : 주문 페이지네이션 적용

--- src/main/kotlin/store/baribari/demo/controller/OrderController.kt
+++ src/main/kotlin/store/baribari/demo/controller/OrderController.kt
@@ -5,12 +5,11 @@ import org.springframework.security.core.userdetails.User
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import store.baribari.demo.auth.LoginUser
-import store.baribari.demo.common.util.log
+import store.baribari.demo.dto.common.ApiResponse
 import store.baribari.demo.dto.order.request.CancelOrderItemResponseDto
 import store.baribari.demo.dto.order.request.CreateOrderRequestDto
 import store.baribari.demo.dto.order.response.FindAllOrderResponseDto
 import store.baribari.demo.dto.order.response.FindOneOrderResponseDto
-import store.baribari.demo.dto.common.ApiResponse
 import store.baribari.demo.service.OrderService
 import javax.validation.Valid
 import javax.validation.constraints.Positive
@@ -28,7 +27,8 @@ class OrderController(
     fun getAllOrderList(
         @LoginUser loginUser: User,
         pageable: Pageable,
-    ): ApiResponse<FindAllOrderResponseDto> {
+
+        ): ApiResponse<FindAllOrderResponseDto> {
         val data = orderService.findAllOrder(loginUser.username, pageable)
 
         return ApiResponse.success(data)

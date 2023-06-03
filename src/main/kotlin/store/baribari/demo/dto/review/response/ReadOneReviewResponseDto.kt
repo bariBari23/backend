64e4af9bba9855932178fd772e6431c06a7788fb
mirageoasis <kimhw0820@naver.com>
2023-06-03 17:21:02 +0900

    refactor : 리뷰 조회중 주문한 아이템 정보 조회 가능하게 변경

--- src/main/kotlin/store/baribari/demo/dto/review/response/ReadOneReviewResponseDto.kt
+++ src/main/kotlin/store/baribari/demo/dto/review/response/ReadOneReviewResponseDto.kt
@@ -1,11 +1,13 @@
 package store.baribari.demo.dto.review.response
 
+import store.baribari.demo.dto.order.response.OrderItemDto
 import store.baribari.demo.model.Review
 import store.baribari.demo.model.User
 
 data class ReadOneReviewResponseDto(
+    // TODO: 도시락도 여기 정보에 넣는 것이 좋아보임
     val reviewId: Long,
-    val orderItemId: Long,
+    val orderItem: OrderItemDto,
     val content: String,
     val rating: Int,
     val photoList: List<String> = listOf(),
@@ -17,7 +19,7 @@ data class ReadOneReviewResponseDto(
         fun fromReview(review: Review, user: User?): ReadOneReviewResponseDto {
             return ReadOneReviewResponseDto(
                 reviewId = review.id!!,
-                orderItemId = review.orderItem.id!!,
+                orderItem = OrderItemDto.fromOrderItem(review.orderItem),
                 content = review.content,
                 rating = review.rating,
                 photoList = review.photoList,

106a270b54358245dace1850f647c67f7445c916
yumiiiiiii <102947194+yumiiiiiii@users.noreply.github.com>
2023-07-15 20:26:49 +0900

    refactor: 태그 변수명 수정

--- src/main/kotlin/store/baribari/demo/common/enums/ReviewCategory.kt
+++ src/main/kotlin/store/baribari/demo/common/enums/ReviewCategory.kt
@@ -8,7 +8,7 @@ enum class ReviewCategory(
     largeAmount("너무 많아요"),
     badTaste("별로예요"),
     plainTaste("보통이에요"),
-    goddTaste("맛있어요"),
+    goodTaste("맛있어요"),
     badStatus("허술해요"),
     plainStatus("보통이에요"),
     goodStatus("깔끔해요"),

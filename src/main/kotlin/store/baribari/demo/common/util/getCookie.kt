33c4db216383f7698fa3226fcea942078b8df345
mirageoasis <kimhw0820@naver.com>
2023-07-31 16:37:20 +0900

    chore : 배포 롤백

--- src/main/kotlin/store/baribari/demo/common/util/getCookie.kt
+++ src/main/kotlin/store/baribari/demo/common/util/getCookie.kt
@@ -1,7 +1,5 @@
 package store.baribari.demo.common.util
 
-// samesite 설정
-import org.springframework.http.ResponseCookie
 import org.springframework.util.SerializationUtils
 import java.util.*
 import javax.servlet.http.Cookie
@@ -12,24 +10,14 @@ fun getCookie(request: HttpServletRequest, name: String) = request.cookies?.let 
     it.find { cookie -> cookie.name == name }
 }
 
-//fun addCookie(response: HttpServletResponse, name: String, value: String, maxAge: Long) {
-//    val cookie = ResponseCookie.from(name, value)
-//    cookie.path = "/"
-//    cookie.isHttpOnly = true
-//    cookie.secure = true
-//    cookie.maxAge = maxAge.toInt()
-//
-//    response.addCookie(cookie)
-//}
-fun addCookie(response: HttpServletResponse, name: String?, value: String?, maxAge: Long) {
-    val cookie = ResponseCookie.from(name!!, value!!)
-            .path("/")
-            .sameSite("None")
-            .httpOnly(true)
-            .secure(true)
-            .maxAge(maxAge.toLong())
-            .build()
-    response.setHeader("Set-Cookie", cookie.toString())
+fun addCookie(response: HttpServletResponse, name: String, value: String, maxAge: Long) {
+    val cookie = Cookie(name, value)
+    cookie.path = "/"
+    cookie.isHttpOnly = true
+    cookie.secure = true
+    cookie.maxAge = maxAge.toInt()
+
+    response.addCookie(cookie)
 }
 
 fun deleteCookie(request: HttpServletRequest, response: HttpServletResponse, name: String) {
